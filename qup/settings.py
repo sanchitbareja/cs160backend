38f7d03034f5466155e74dc7fb8085c8be37e747
Sid Feygin <sid.feygin@berkeley.edu>
2013-12-08 14:22:16 -0800

    fix settings?

--- qup/settings.py
+++ qup/settings.py
@@ -67,12 +67,6 @@ WSGI_APPLICATION = 'qup.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 
 
